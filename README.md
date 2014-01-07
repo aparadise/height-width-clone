2f7647133c7b4cfc826cb324b4b6624dc69ff0f8
chris <chris@bumblehead.com>
2014-01-06 20:53:31 -0800

    videos will now display!

--- README.md
+++ README.md
@@ -7,3 +7,32 @@ I have wrapper that loads dynamic video feeds from a provider.<br>
 But sometimes the video is larger then the wrapper or it's not taking up the full height and width leaving a gap of space.<br>
 so to fix this issue if you know the id or class of the child-element you can force the child element to take up the full space of the wrapper
 having a nice clean container.
+
+------
+### Install
+
+```bash
+$ git clone https://github.com/aparadise/height-width-clone.git
+```
+
+------
+### Testing
+
+Load the ./test/index.html and click 'Load Video'.
+
+MSN videos will not appear for pages loaded through file protocol (file:///), so there's some additional setup.
+
+Use Apache, which comes with OSX:
+
+ 1. Create a symlink to the cloned repository
+    ```bash
+    $ sudo ln -s /Users/chrisdep/Software/height-width-clone /Library/WebServer/Documents/height-width-clone
+    ```
+
+ 2. Start Apache
+    ```bash
+    sudo apachectl start
+    ```
+
+ 3. Request 'localhost/test/' with your browser
+
--- test/widthheightvideotest.js
+++ test/widthheightvideotest.js
@@ -2,67 +2,19 @@
 var widthheightvideotest = (function (proto) {
 
   proto = {
-    getAsVideoHTML : function (opts) {
-
-      console.log('opts', opts);
-      var videoHTML = '' +
-        '<video id="video" class="video"' +
-        '       controls preload="auto" width="640" height="264">' +
-        '  <source src=":srcMp4" type="video/mp4" />' +
-        '</video>';
-
-      videoHTML = videoHTML.replace(/:srcMp4/, opts.mp4);
-
-      return videoHTML;
-      //console.log('videoHTML', videoHTML, opts);
-      /*
-      <video id="example_video_1" class="video-js vjs-default-skin"
-      controls preload="auto" width="640" height="264"
-      poster="http://video-js.zencoder.com/oceans-clip.png"
-      data-setup='{"example_option":true}'>
-        <source src="http://video-js.zencoder.com/oceans-clip.mp4" type='video/mp4' />
-       */
-   },
-
     loadResponsiveVideo : function () {
-      var that = this;
-      var tplUrl = 'http://edge2.catalog.video.msn.com/relatedVideos.aspx?uuids=383b5802-e9a4-414c-96fb-0ab1f7c0db65&ff=82&rct=1,3,5,6&mk=us&alg=0&ps=16';
+      var options = {
+        playListQuery : "videobyuuids.aspx?uuids=b464b082-6561-47c2-9d01-f2b0ba810172", 
+        mostViewedPlayListQuery: "videobytag.aspx?mk=us&vs=2&ns=Fox%20Sports_Gallery&tag=top%20news&ipt=1&responseEncoding=json", 
+        autoplay: true
+      };
 
-      $.ajax({
-	url: tplUrl,
-	error: function(e) {
-	  //renderResults(e.responseText);
-	},
-	success: function (data) {
-          // once loaded... several new namespaces are be defined
-          // MsnVideo2, MsnVideo, MsnVideoUX*
-
-          var videoDataObj = simpleconvertxml.getXMLAsObj(data);
-
-          var sampleMP4 = videoDataObj.videos.video[0].videoFiles.videoFile[0].uri;
-          var videoHTML = that.getAsVideoHTML({ 
-            mp4 : sampleMP4 
-          });
-
-          MsnVideoUx.render("FSRedesign", "FSFeaturedVideo", {
-            "FSVideoWidgetPlayer.DefaultVideo" : "videobytag.aspx?mk=us&vs=2&ns=Fox%20Sports_Gallery&tag=top%20news&ipt=1",
-            "FSVideoWidgetPlayer.DefaultPlaylist" : "videobytag.aspx?mk=us&vs=2&ns=Fox%20Sports_Gallery&tag=top%20news&ipt=1&responseEncoding=json",
-            "FSVideoWidgetPlayer.AutoPlayVideo" : true
-          }, {
-            csid : "ux-cms-en-us-sports"
-          });
-
-          var videoContainerElem = document.getElementById('FSFeaturedVideo');
-          if (videoContainerElem) {
-          
-            console.log('videoHTML', videoHTML, videoContainerElem);
-            //videoContainerElem.innerHTML = videoHTML;
-            //videoContainerElem.appendChild(data);
-          }
-        },
-	complete: function(e) {
-	  //  console.log("completed, ", e);
-	}
+      MsnVideoUx.render("FSRedesign", "FSFeaturedVideo", {
+        "FSVideoWidgetPlayer.DefaultVideo" : options.playListQuery,
+        "FSVideoWidgetPlayer.DefaultPlaylist" : options.mostViewedPlayListQuery,
+        "FSVideoWidgetPlayer.AutoPlayVideo" : options.autoplay
+      }, {
+        csid : "ux-cms-en-us-sports"
       });
     },
 
@@ -78,8 +30,12 @@ var widthheightvideotest = (function (proto) {
     connect : function () {
       var that = this;
 
-      $('.b').on("click", function () {
-         that.run();
+      $('#ResizeVideoLink').on("click", function () {
+        that.run();
+      });
+
+      $('#LoadVideoLink').on('click', function () {
+        that.loadResponsiveVideo();
       });
     }
   };
@@ -88,7 +44,6 @@ var widthheightvideotest = (function (proto) {
     init : function () {
       var that = Object.create(proto);
 
-      that.loadResponsiveVideo();
       that.connect();
 
       return that;
